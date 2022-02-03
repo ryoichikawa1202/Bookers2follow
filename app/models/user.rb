5d6cd233e9667600c1ff0eab5c07aace328d9280
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-02-03 07:45:44 +0000

    [finish]応用課題5

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,13 +36,13 @@ class User < ApplicationRecord
 
   def self.looks(search,word)
     if search == "perfect_match"
-      @user = User.where("name LIKE?", "#{word}")
+      @user = User.where("name LIKE ?", "#{word}")
     elsif search == "forward_match"
-      @user = User.where("name LIKE?", "#{word}%")
+      @user = User.where("name LIKE ?", "#{word}%")
     elsif search == "backward_match"
-      @user = User.where("name LIKE?","%#{word}")
+      @user = User.where("name LIKE ?","%#{word}")
     elsif search == "partial_match"
-      @user = User.where("name LIKE?","%#{word}%")
+      @user = User.where("name LIKE ?","%#{word}%")
     else
       @user = User.all
     end
